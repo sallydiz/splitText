0d9eba1e8d73fceb54cbf89dc826ad605064a5a9
Sal Sidani <79154128+sallydiz@users.noreply.github.com>
2021-03-22 22:10:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -229,7 +229,7 @@
     var r = n(0),
       o = n.n(r);
 
-      o()(document.querySelector('#SplitText'), {
+      o()(document.querySelector('#TxtRvl'), {
         markup:
           '<div class="split_outer"><div class="split_inner">{slot}</div></div>',
       })
